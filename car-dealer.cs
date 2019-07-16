2ab24ce3d1e42e6e053f681fa6e13217f8f9609d
Student <example@example.com>
2019-07-16 09:58:57 -0700

    Add detailed comments to sections of logic.

--- car-dealer.cs
+++ car-dealer.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 
+
+// Car class that defines the constructor
 class Car
 {
   public string MakeModel;
   public int Price;
   public int Miles;
-
+// Constructor logic that is a function, and takes the information from constructions later and adds those values to the pre-determined values above.
   public Car(string makeModel, int price, int miles)
   {
     MakeModel = makeModel;
@@ -14,29 +16,32 @@ class Car
     Miles = miles;
   }
 
+// Function to determine if Price of car is less than maxPrice (True/false)
   public bool WorthBuying(int maxPrice)
   {
     return (Price < maxPrice);
   }
 }
-
+// What calls the logic in the backend (above, I assume) and interacts with user.
 public class Program
 {
   public static void Main()
   {
+    //   Construction of cars. 
     Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
     Car yugo = new Car("1980 Yugo Koral", 700, 56000);
     Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
     Car amc = new Car("1976 AMC Pacer", 400, 198000);
-
+    // List containing all instances of the Car objects (that have been defined)
     List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
 
     Console.WriteLine("Enter maximum price: ");
     string stringMaxPrice = Console.ReadLine();
     int maxPrice = int.Parse(stringMaxPrice);
-
+    // Empty list to be filled with search criteria.
     List<Car> CarsMatchingSearch = new List<Car>(0);
 
+    // foreach loop that adds cars that meet price criteria to above list
     foreach (Car automobile in Cars)
     {
       if (automobile.WorthBuying(maxPrice))
@@ -44,7 +49,7 @@ public class Program
         CarsMatchingSearch.Add(automobile);
       }
     }
-
+    // foreach that displays the search critera above to user.
     foreach(Car automobile in CarsMatchingSearch)
     {
       Console.WriteLine(automobile.MakeModel);
